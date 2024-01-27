93b981b6d5828e2019b2133752e3fe9c8f0594fb
Simon Hischier <simon_hischier@hotmail.com>
2024-01-27 17:29:50 +0100

    Platform should work now

---
+++ GGJ_2024_Prototype/Assets/MovingJelly/PlatformTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlatformTrigger : MonoBehaviour
+{
+
+    public void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.transform.parent = gameObject.transform;
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        other.gameObject.transform.parent = null;
+    }
+}
